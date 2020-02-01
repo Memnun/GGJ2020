4a5194116e60496a196ba42b6ef94792020f9afb
Memnun <zeronocool@gmail.com>
2020-01-31 21:59:31 -0500

    starting scripts

---
+++ GGJ2020Proj/Assets/Scripts/DropShadow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropShadow : MonoBehaviour
+{
+
+    public Material ShadowMaterial;
+    public SpriteRenderer Silhouette;
+    public Vector2 ShadowOffset;
+    private GameObject shadowSprite;
+    
+    void Start()
+    {
+        shadowSprite = new GameObject("shadow");
+        SpriteRenderer shadowSpriteRenderer = shadowSprite.AddComponent<SpriteRenderer>();
+        shadowSpriteRenderer.sprite = Silhouette.sprite;
+        shadowSpriteRenderer.material = ShadowMaterial;
+        
+        shadowSpriteRenderer.sortingLayerName = Silhouette.sortingLayerName;
+        shadowSpriteRenderer.sortingOrder = Silhouette.sortingOrder - 1;
+        
+    }
+    
+    void LateUpdate()
+    {
+        shadowSprite.transform.localPosition = transform.localPosition + (Vector3)ShadowOffset;
+        shadowSprite.transform.localRotation = transform.localRotation;
+    }
+}
---
+++ GGJ2020Proj/Assets/Scripts/PlayerConstants.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerConstants
+{
+
+    public int Cash;
+    public int Swings;
+    public List<int> Furniture;
+    public List<int> Upgrades;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cash = 100;
+        Swings = 0;
+        Furniture = new List<int>();
+        Upgrades = new List<int>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
